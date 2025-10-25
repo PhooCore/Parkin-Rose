efea80295a932cf4d989136b094f12bed752c2b0
pho29 <tuyet.nguyen@etu.iut-tlse3.fr>
2025-10-25 15:25:20 +0200

    connexion mysql

--- SAE_PARKIN_ROSE/src/main/java/dao/MySQLConnection.java
+++ SAE_PARKIN_ROSE/src/main/java/dao/MySQLConnection.java
@@ -1,15 +1,25 @@
 package dao;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class MySQLConnection {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/sae_parking";
-    private static final String USER = "root";
-    private static final String PASSWORD = "SAEparking7";
+    // Pour l'accès distant - à partager avec les autres
+    private static final String URL = "jdbc:mysql://2a01:cb19:8965:5100:91ba:1016:385c:67c1:3306/parkinrosebdd";
+    private static final String USER = "admin_parkinrose";
+    private static final String PASSWORD = "$iutinfo";
+    
     
     public static Connection getConnection() throws SQLException {
+        try {
+            // Charger le driver MySQL
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("Driver MySQL non trouvé", e);
+        }
+        
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
